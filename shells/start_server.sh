edecc886a4616be1b8a862228d665ebdca296b67
Dario Velez <dvelezroman@gmail.com>
2021-10-04 01:37:12 -0300

    Merged fix/cd-ci into develop

--- shells/start_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Go to app folder
-cd /home/ubuntu/Sites/ngine
-
-# start again
-pm2 start all
--- shells/stop_server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Go to app folder
-cd /var/www/html
-
-# stop running process
-pm2 stop all
