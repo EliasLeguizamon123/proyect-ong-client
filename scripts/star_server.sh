94680d63248bf907ea42219437ebeffdc0b88cfc
Dario Velez Roman <dvelezroman@gmail.com>
2021-10-03 23:00:08 -0300

    refactor the CI / CD pipelines

---
+++ scripts/star_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Go to app folder
+cd /home/ubuntu/Sites/ngine
+
+# start again
+pm2 start all
---
+++ scripts/stop_server.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Go to app folder
+cd /var/www/html
+
+# stop running process
+pm2 stop all
