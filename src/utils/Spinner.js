ceba8166e500e6d2fea6426f100c6972ba675755
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2021-10-05 14:11:43 +0000

    Merged in OT75-19 (pull request #4)
    Add library react-loader-spinner and set spinner for client repo
    
    * Add library react-loader-spinner and set spinner for client repo
    
    * add props to configure loader
    
    Approved-by: Dario Velez
    Approved-by: Fabian Liuni

---
+++ src/utils/Spinner.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import Loader from 'react-loader-spinner';
+
+export default function Spinner({ type, color = 'C0C0C0', height, width, timeout }) {
+  return <Loader type={type} color={color} height={height} width={width} timeout={timeout} />;
+}
