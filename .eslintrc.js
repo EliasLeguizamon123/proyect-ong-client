924aa81bfe25a0ad8afb1d65b78d76d9427746d4
Dario Velez <dvelezroman@gmail.com>
2021-10-05 19:57:37 -0300

    Merged pipeline-alt into develop

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		node: true
+	},
+	settings: {
+    react: {
+      version: "detect"
+    }
+  },
+	parserOptions: {
+		parser: 'babel-eslint',
+		ecmaVersion: 2018,
+		sourceType: 'module'
+	},
+	extends: [
+		'plugin:react/recommended',
+		'plugin:prettier/recommended'
+	],
+	plugins: [],
+	// add your custom rules here
+	rules: {
+			"react/prop-types": 1
+	}
+}
--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.render(
       </ChakraProvider>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
