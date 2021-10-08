d5cd9e9111336b82f74121760c42c4a705193cd8
Fabian Liuni <fabianliuni@gmail.com>
2021-10-08 00:48:23 +0000

    Merged in OT75-20 (pull request #12)
    OT75-20
    
    * Added dependency
    
    * Added alert helper functions
    
    * Added link to documentation
    
    * Undo changes in dummy component about to get erased.
    
    
    Approved-by: Dario Velez
    Approved-by: Ruben Gianelli

---
+++ src/utils/alerts.js
@@ -0,0 +1,14 @@
+import Swal_class from 'sweetalert2'
+
+export const alertError = (title, message) => {
+  Swal_class.fire(title, message, 'error')
+}
+
+export const alertSuccess = (title, message) => {
+  Swal_class.fire(title, message, 'success')
+}
+
+// Exports the entire class to be able to use all customizations
+// For list of different options, find documentation at:
+// https://sweetalert2.github.io/
+export const Swal = Swal_class
