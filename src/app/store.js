af679f4397d157e10795cdb93e28130ec2ce3c12
Fabian Liuni <fabianliuni@gmail.com>
2021-10-14 18:27:53 +0000

    Merged in OT75-43 (pull request #29)
    Added reducer to handle user state and subscription in store to handle data persistance
    
    * Added reducer to handle user state and subscription in store to handle data persistance
    
    
    Approved-by: Dario Velez
    Approved-by: Elias Leguizamon

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit'
+import userReducer from '../features/user/userSlice'
 
-export default configureStore({
+const store = configureStore({
   reducer: {
-    counter: null,
+    user: userReducer,
   },
 })
+
+let currentAuthState
+// In this function we handle the side effects of setting and deleting the local storage
+function handleChange() {
+  const state = store.getState()
+
+  const previousAuthState = currentAuthState
+  currentAuthState = state.user.authenticated
+
+  // We will only act if the authenticated property changed
+  if (previousAuthState !== currentAuthState) {
+    if (currentAuthState === false) localStorage.removeItem('token')
+    else localStorage.setItem('token', state.user.token)
+  }
+}
+
+store.subscribe(handleChange)
+
+export default store
