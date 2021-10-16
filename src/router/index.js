902d045ed492eadfcae870b3942cdf4e8a5eb4f0
Ruben Gianelli <rubengianelli8@gmail.com>
2021-10-16 20:03:32 +0000

    Merged in OT75-104 (pull request #41)
    OT75-104
    
    * import modules with lazy
    
    * Merge branch 'develop' of https://bitbucket.org/alkemy-dev/t75-client into OT75-104
    
    * Merge branch 'develop' into OT75-104
    
    
    Approved-by: Dario Velez
    Approved-by: Martin de Amorin
    Approved-by: Elias Leguizamon

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Home from '../screen/Home'
-import About from '../screen/About'
-import RegisterPage from '../screen/RegisterPage'
-import LoginPage from '../screen/LoginPage'
-import S3Test from '../screen/S3Test'
-import TestimonialsForm from '../components/TestimonialsForm/TestimonialsForm'
-import SlideForm from '../components/SlideForm'
-import ActivitiesForm from '../components/ActivitiesForm/ActivitiesForm'
-import ActivityDetail from '../screen/ActivityDetail'
-import NewsPage from '../screen/NewsPage'
-import NewsDetail from '../screen/NewsDetail'
-import Footer from '../components/footer'
-import NewsForm from '../components/News/NewsForm'
 import Header from '../components/header'
-import ContactPage from '../screen/ContactPage'
-import CategoriesForm from '../components/Categories/CategoriesForm'
-import NewsListEdit from '../screen/NewsListEdit'
-import BackContactPage from '../screen/BackContactPage'
-import Categories from '../components/Categories/Categories'
-import PrivateRoute from './PrivateRouter'
-import Members from '../components/Members/Members'
+import Spinner from '../utils/Spinner'
+
+const getLazyComponent = (path) => lazy(() => import(path))
+
+const Home = getLazyComponent('../screen/Home')
+const About = getLazyComponent('../screen/About')
+const RegisterPage = getLazyComponent('../screen/RegisterPage')
+const LoginPage = getLazyComponent('../screen/LoginPage')
+const TestimonialsForm = lazy(() =>
+  import('../components/TestimonialsForm/TestimonialsForm')
+)
+const ActivitiesForm = lazy(() =>
+  import('../components/ActivitiesForm/ActivitiesForm')
+)
+const ActivityDetail = getLazyComponent('../screen/ActivityDetail')
+const NewsPage = getLazyComponent('../screen/NewsPage')
+const NewsDetail = getLazyComponent('../screen/NewsDetail')
+const NewsForm = getLazyComponent('../components/News/NewsForm')
+const Footer = getLazyComponent('../components/footer')
+const ContactPage = getLazyComponent('../screen/ContactPage')
+const BackContactPage = getLazyComponent('../screen/BackContactPage')
+const Categories = getLazyComponent('../components/Categories/Categories')
+
+const SlideForm = getLazyComponent('../components/SlideForm')
+const CategoriesForm = getLazyComponent(
+  '../components/Categories/CategoriesForm'
+)
+const NewsListEdit = getLazyComponent('../screen/NewsListEdit')
+const PrivateRoute = getLazyComponent('./PrivateRouter')
+const Members = getLazyComponent('../components/Members/Members')
 
 export default function Router() {
   return (
@@ -35,41 +45,39 @@ export default function Router() {
           { name: 'login', path: '/login' },
         ]}
       />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/register" component={RegisterPage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/novedades" component={NewsPage} />
-        <Route exact path="/novedades/:id" component={NewsDetail} />
-        <Route exact path="/backoffice/novedades" component={NewsListEdit} />
-        <Route exact path="/actividades/:id" component={ActivityDetail} />
-        <Route exact path="/s3test" component={S3Test} />
-        <Route path="/testimonials/:id?" component={TestimonialsForm} />
-        <Route path="/users/:id?" component={EditUserForm} />
-        <PrivateRoute
-          path="/backoffice/contactos"
-          component={BackContactPage}
-        />
-        <Route path="/contacto" component={ContactPage} />
-        <Route path="/activities/:id?" component={ActivitiesForm} />
-        <PrivateRoute path="/backoffice/categories" component={Categories} />
-        <Route path="/slides" component={SlideForm} />
-        <Route path="/contact" component={ContactPage} />
-        <Route path="/novedad/:id?" component={NewsForm} />
-        <Route path="/backoffice/contactos" component={BackContactPage} />
-        <Route path="/contacto" component={ContactPage} />
-        <Route path="/activities/:id?" component={ActivitiesForm} />
-        <Route path="/backoffice/categories" component={Categories} />
-        <Route path="/users/:id?" component={EditUserForm} />
-        <Route
-          path="/backoffice/categories/form/:id"
-          component={CategoriesForm}
-        />
-        <Route path="/backoffice/categories/form" component={CategoriesForm} />
-        <Route path="/nosotros" component={Members} />
-      </Switch>
-      <Footer />
+      <Suspense fallback={<Spinner />}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/register" component={RegisterPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/novedades" component={NewsPage} />
+          <Route exact path="/novedades/:id" component={NewsDetail} />
+          <Route path="/novedad/:id?" component={NewsForm} />
+          <Route exact path="/actividades/:id" component={ActivityDetail} />
+          <Route exact path="/s3test" component={S3Test} />
+          <Route path="/testimonials/:id?" component={TestimonialsForm} />
+          <Route path="/backoffice/contactos" component={BackContactPage} />
+          <Route path="/contacto" component={ContactPage} />
+          <Route path="/activities/:id?" component={ActivitiesForm} />
+          <Route path="/backoffice/categories" component={Categories} />
+          <Route exact path="/backoffice/novedades" component={NewsListEdit} />
+          <Route path="/users/:id?" component={EditUserForm} />
+          <Route path="/slides" component={SlideForm} />
+          <Route path="/nosotros" component={Members} />
+          <Route
+            path="/backoffice/categories/form/:id"
+            component={CategoriesForm}
+          />
+          <Route
+            path="/backoffice/categories/form"
+            component={CategoriesForm}
+          />
+
+          <PrivateRoute path="/backoffice/categories" component={Categories} />
+        </Switch>
+        <Footer />
+      </Suspense>
     </BrowserRouter>
   )
 }
